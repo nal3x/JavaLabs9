4622d5e313ff0e3b48719af9afe1987427f116a0
nalex <nalex@e7450>
2017-12-04 03:01:07 +0200

    .

--- src/Division.java
+++ src/Division.java
@@ -9,20 +9,47 @@ public class Division {
                     "Πληκτρολογήστε έναν πραγματικό αριθμό για να διαιρέσει το 100",
                     "Είσοδος αριθμού",
                     JOptionPane.QUESTION_MESSAGE);
+            try {
+                double diereteos = (new Double(input)).doubleValue();
+                String result = dieretis + " / " + diereteos + " = " + 
+                divide(dieretis,diereteos);
 
-            double diereteos = (new Double(input)).doubleValue();
-            String result = dieretis + " / " + diereteos + " = " + divide(dieretis,diereteos);
-        
-            result += "\n\n Θέλετε να εκτελέσετε και άλλη διαίρεση;";
-                    
-// response = 0 αν ο χρήστης απαντήσει 'Yes' ενώ response = 1 αν ο χρήστης 
-// απαντήσει 'No'
+                result += "\n\n Θέλετε να εκτελέσετε και άλλη διαίρεση;";
 
-            response = JOptionPane.showConfirmDialog(null, result, "Αποτέλεσμα διαίρεσης", JOptionPane.YES_NO_OPTION);
-        }while (response == 0);       
-    }
+                response = JOptionPane.showConfirmDialog(null,
+                        result,
+                        "Αποτέλεσμα διαίρεσης",
+                        JOptionPane.YES_NO_OPTION);
+            }
+            catch (NullPointerException np) { 
+            response = 1; //  Έτσι θα προκαλέσουμε την έξοδο του προγράμματος   
+            }
+            catch (NumberFormatException nfe) {
+                // Ο χρήστης πληκτρολόγησε κάτι που ΔΕΝ είναι πραγματικός αριθμός
+                JOptionPane.showMessageDialog(null,
+                        "Το " + input + " δεν είναι δεκαδικός αριθμός!!",
+                        "Λάθος είσοδος",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+            catch (IllegalArgumentException iae) {
+                //Διαχειρίζεται την εξαίρεση από την μέθοδο divide
+                JOptionPane.showMessageDialog(null, 
+                        iae.toString(),
+                        "Λάθος είσοδος",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } while (response == 0);      
+}
 
-    public static double divide (double n1, double n2) {
-        return n1/n2;
-    }
+
+    /* Σε περίπτωση που η δεύτερη παράμετρος (ο διαιρετέος) είναι μηδέν τότε
+    δε θα εκτελεστεί η διαίρεση αλλά θα "πετάξει" μια IllegalArgumentException
+    */
+
+    public static double divide (double n1, double n2) throws IllegalArgumentException {
+        if (n2 != 0)
+            return n1/n2;
+        else
+            throw new IllegalArgumentException("Αδύνατη η διαίρεση με το μηδέν");
+        }
 }
